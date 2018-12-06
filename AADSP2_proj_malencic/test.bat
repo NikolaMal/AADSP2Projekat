050ddde75eb0edd338c00de0fe38267850a1f86d
Nikola <nmalencic@gmail.com>
2018-12-06 10:19:38 +0100

    nznm

--- AADSP2_proj_malencic/test.bat
+++ AADSP2_proj_malencic/test.bat
@@ -1,3 +1,7 @@
-cd C:\Users\Nikola\Documents\fax\AADSP2-sve\AADSP2_proj_malencic\model0\Debug
+cd C:\Users\Nikola\Documents\Visual Studio 2015\Projects\model0\Debug
 
 "model0.exe" C:\Users\Nikola\Documents\fax\AADSP2-sve\TestStreams\radio-44100hz-16bit-mono.wav C:\Users\Nikola\Documents\fax\AADSP2-sve\TestStreams\OutStreams\outModel0.wav 2 0.5
+
+cd C:\Users\Nikola\Documents\Visual Studio 2015\Projects\model1\Debug
+
+"model1.exe" C:\Users\Nikola\Documents\fax\AADSP2-sve\TestStreams\radio-44100hz-16bit-mono.wav C:\Users\Nikola\Documents\fax\AADSP2-sve\TestStreams\OutStreams\outModel1.wav 2 0.5
